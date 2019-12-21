28963272103fae8c4e017aabb6c04af6a593eddc
GeonHo Tony Han <hrg921@gmail.com>
2019-12-22 03:18:38 +0900

    make works first spec

--- src/decision-maker.spec.ts
+++ src/decision-maker.spec.ts
@@ -3,5 +3,5 @@ import { DecisionMaker, MORE_THAN_THREE_OPTIONS_NEEDED_ERROR_MESSAGE } from './d
 it('DecisionMaker should be initialized with more than three options, if not throw error', () => {
     const decisionMaker = new DecisionMaker();
     const options = [undefined, undefined];
-    expect(() => DecisionMaker.initialize({ options })).toThrowError(MORE_THAN_THREE_OPTIONS_NEEDED_ERROR_MESSAGE)
+    expect(() => decisionMaker.initialize({ options })).toThrowError(MORE_THAN_THREE_OPTIONS_NEEDED_ERROR_MESSAGE)
 });
--- src/decision-maker.ts
+++ src/decision-maker.ts
@@ -1,1 +1,13 @@
 export const MORE_THAN_THREE_OPTIONS_NEEDED_ERROR_MESSAGE = 'There must be more than three options'
+
+interface InitializeArgs {
+    options: any[]
+}
+
+export class DecisionMaker {
+    public initialize({ options }: InitializeArgs) {
+        if (options.length < 3) {
+            throw Error(MORE_THAN_THREE_OPTIONS_NEEDED_ERROR_MESSAGE)
+        }
+    }
+}
