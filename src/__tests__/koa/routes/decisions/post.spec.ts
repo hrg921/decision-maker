cf8f64dd20e28c64d286ffae89936d38450afbab
GeonHo Han <hrg921@gmail.com>
2019-12-28 20:06:21 +0900

    test: add faker title test

--- src/__tests__/koa/routes/decisions/post.spec.ts
+++ src/__tests__/koa/routes/decisions/post.spec.ts
@@ -1,13 +1,16 @@
+import faker from 'faker';
 import request from 'supertest';
 import { app } from '~koa/app';
 
 describe("/decisions post requests", () => {
   it("should create a new decision with title", async () => {
+    const title = faker.lorem.words(4);
     const res = await request(app.callback())
       .post("/decisions")
-      .send({ title: "new decision" });
+      .send({ title });
     expect(res.status).toEqual(200);
     expect(res.body).toHaveProperty("id");
     expect(res.body).toHaveProperty("decision");
+    expect(res.body.decision.title).toEqual(title);
   });
 });
