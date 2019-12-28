1cc2f4fc0c4925d1866bc1e21fe6bf10693f7edd
GeonHo Han <hrg921@gmail.com>
2019-12-28 20:13:19 +0900

    test: add persistency test

--- src/__tests__/koa/routes/decisions/post.spec.ts
+++ src/__tests__/koa/routes/decisions/post.spec.ts
@@ -5,12 +5,18 @@ import { app } from '~koa/app';
 describe("/decisions post requests", () => {
   it("should create a new decision with title", async () => {
     const title = faker.lorem.words(4);
-    const res = await request(app.callback())
+    const postResponse = await request(app.callback())
       .post("/decisions")
       .send({ title });
+    const { id } = postResponse.body.decision;
+
+    const res = await request(app.callback())
+      .get(`/decisions/${id}`)
+      .send();
+
     expect(res.status).toEqual(200);
-    expect(res.body).toHaveProperty("id");
     expect(res.body).toHaveProperty("decision");
+    expect(res.body.decision.id).toEqual(id);
     expect(res.body.decision.title).toEqual(title);
   });
 });
